56b942249f59afe554fedb20fd419513499ac99f
Arthi K Rajan <43614625+Arthi01@users.noreply.github.com>
2020-06-07 15:14:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
 ##Please include your own comment to explain your code (Required in Rubric)
 
 makeCacheMatrix <- function(x = matrix()) {
-  j <- NULL
+  inv <- NULL
   set <- function(y){
   x <<- y
-  j <<- NULL
+  inv <<- NULL
   }
   get <- function()x
-  setInverse <- function(inverse) j <<- inverse
-  getInverse <- function() j 
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv 
   list(set = set, get = get, 
   setInverse = setInverse, 
   getInverse = getInverse)
@@ -18,13 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
-  j <- x$getInverse()
-  if(!is.null(j)){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
   message("getting cached data")
-  return(j)
+  return(inv)
   }
   mat <- x$get()
-  j <- solve(mat,...)
-  x$setInverse(j)
-  j
+  inv <- solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
